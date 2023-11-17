91e027342b3f035d9f8a5ab54565b7c41995ba2a
Nacho <Oliveirawillian72@gmail.com>
2023-11-17 12:20:54 -0300

    q
    dw

--- levels/delicious_cake/script.c
+++ levels/delicious_cake/script.c
@@ -40,7 +40,7 @@ const LevelScript level_delicious_cake_entry[] = {
 		OBJECT(MODEL_STAR, -6, 1964, -2, 0, 0, 0, 0x00000000, bhvStar),
 		TERRAIN(delicious_cake_area_1_collision),
 		MACRO_OBJECTS(delicious_cake_area_1_macro_objs),
-		SET_BACKGROUND_MUSIC(0x00, SEQ_LEVEL_GRASS),
+		SET_BACKGROUND_MUSIC(0x00, SEQ_SOUND_PLAYER),
 		TERRAIN_TYPE(TERRAIN_GRASS),
 		/* Fast64 begin persistent block [area commands] */
 		/* Fast64 end persistent block [area commands] */
